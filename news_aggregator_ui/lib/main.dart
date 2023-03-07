d1b4bafdd82f20ae63fe20fa95522c43d4e25843
Mar <up2079248@myport.ac.uk>
2023-03-07 16:41:58 +0000

    Issue_001_ui Fixes #1

--- news_aggregator_ui/lib/main.dart
+++ news_aggregator_ui/lib/main.dart
@@ -1,51 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:news_aggregator_ui/home_page.dart';
 
 void main() {
-  runApp(const NewsAggregator());
-}
-
-class NewsAggregator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Start Page',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home:LoginPage(),
-    );
-  }
+  runApp(MaterialApp(
+    title: 'News Agreggator',
+    home: LoginPage(),
+  ));
 }
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _LoginPageState createState() => new _LoginPageState();
 }
 
+Color myGreen = Color.fromARGB(255, 129, 197, 129);
+
 class _LoginPageState extends State<LoginPage> {
+  // Initialize variables to store email and password
+  String _email = '';
+  String _password = '';
+
+  // Method to handle login button press
+  void _handleLogin() {
+    // Check if email and password are not empty
+    if (_email.isNotEmpty && _password.isNotEmpty) {
+      // Navigate to main page of the app
+      Navigator.pushNamed(context, '/main');
+    } else {
+      // Show error message if email or password are empty
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Please enter your email and password.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              ),
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: myGreen,
       appBar: AppBar(
-        title: Text('Login Page'),
+        backgroundColor: myGreen,
+        title: Text('News Agreggator App'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: mainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 300.0,
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  border: OutlineInputBorder(),
-                ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Email input field
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'example@gmail.com',
               ),
+              onChanged: (value) => setState(() => _email = value),
             ),
-            SizedBox(height: 20.0),
-            RaisedButton(
-              onPressed: () {},
+            SizedBox(height: 16.0),
+
+            // Password input field
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: '***************',
+              ),
+              onChanged: (value) => setState(() => _password = value),
+            ),
+            SizedBox(height: 32.0),
+
+            // Login button
+            ElevatedButton(
+              onPressed: _handleLogin,
+              style: ElevatedButton.styleFrom(backgroundColor: myGreen),
               child: Text('Login'),
             ),
           ],
