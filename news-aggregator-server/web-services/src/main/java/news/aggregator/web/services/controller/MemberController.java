1478c2f27b03f4302555ef6f5329f64697eac27c
Jakub Mościński <jakub.moscinski@jwmenergia.pl>
2023-04-03 22:50:07 +0100

    Issue #004_s: Member Controller implemented

--- news-aggregator-server/web-services/src/main/java/news/aggregator/web/services/controller/MemberController.java
+++ news-aggregator-server/web-services/src/main/java/news/aggregator/web/services/controller/MemberController.java
@@ -1,5 +1,32 @@
 package news.aggregator.web.services.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import news.aggregator.flows.model.Member;
+import news.aggregator.flows.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/member")
+@Tag(name = "Member resource", description = "Member controller")
 public class MemberController {
+    private final MemberService service;
+
+    @Autowired
+    public MemberController(@Qualifier("memberService") final MemberService service) {
+        this.service = service;
+    }
+
+    @Operation(summary = "List members")
+    @GetMapping(value = "/list", produces = { MediaType.APPLICATION_JSON_VALUE} )
+    public Flux<Member> list() {
+        return Flux.fromIterable(this.service.findMembers());
+    }
 
 }
