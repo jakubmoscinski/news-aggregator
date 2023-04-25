68b246505588eb99390b13f616acb043bbf0c332
Jakub Moscinski <62364088+jakubmoscinski@users.noreply.github.com>
2023-04-25 13:58:00 +0100

    Issue 007_s user authentication (#25)
    Co-authored-by: Jakub Mościński <jakub.moscinski@jwmenergia.pl>

--- news-aggregator-server/web-services/src/main/java/news/aggregator/web/services/controller/MemberController.java
+++ news-aggregator-server/web-services/src/main/java/news/aggregator/web/services/controller/MemberController.java
@@ -3,19 +3,22 @@ package news.aggregator.web.services.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import news.aggregator.flows.model.Member;
+import news.aggregator.flows.model.UserAuthentication;
 import news.aggregator.flows.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/member")
 @Tag(name = "Member resource", description = "Member controller")
 public class MemberController {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberController.class);
+
     private final MemberService service;
 
     @Autowired
@@ -27,6 +30,12 @@ public class MemberController {
     @GetMapping(value = "/list", produces = { MediaType.APPLICATION_JSON_VALUE} )
     public Flux<Member> list() {
         return Flux.fromIterable(this.service.findMembers());
+    }
+
+    @Operation(summary = "Authenticate")
+    @PostMapping(value = "/authenticate", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void authenticate(@RequestBody final UserAuthentication authentication) {
+        LOG.info("Authentication request");
     }
 
 }
