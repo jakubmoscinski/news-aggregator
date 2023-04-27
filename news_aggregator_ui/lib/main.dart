bd4a4415df4c7b18d4a81f5170ec8fec3b81ac03
Mar <up2079248@myport.ac.uk>
2023-04-27 16:32:25 +0100

    New login page. Common styling for futher views.

--- news_aggregator_ui/lib/main.dart
+++ news_aggregator_ui/lib/main.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:news_aggregator_ui/view/login_view.dart';
+import 'package:news_aggregator_ui/view/authentication_view.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'News Aggregator',
-    home: const MyHomePage(),
-    theme: ThemeData(
-      primaryColor: Colors.grey,
-      textTheme: const TextTheme(
-        titleLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-        bodyLarge: TextStyle(fontSize: 16),
-        labelLarge: TextStyle(color: Colors.white),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.black),
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.grey),
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        labelStyle: const TextStyle(color: Colors.grey),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.black,
-        ),
-      ),
-    ),
-  ));
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News Aggregator',
+      theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateColor.resolveWith((states) => Colors.white),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                padding: MaterialStateProperty.resolveWith(
+                    (states) => const EdgeInsets.all(2))),
+          )),
+      home: const AuthenticationView(),
+    );
+  }
 }
---
+++ news_aggregator_ui/lib/view/authentication_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AuthenticationView extends StatelessWidget {
+  const AuthenticationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userNameController = TextEditingController();
+    final passwordNameController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News Aggregator'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome!',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            _provideTextField('Username', userNameController, false),
+            _provideTextField('Password', passwordNameController, true),
+            ElevatedButton(
+              onPressed: () async {
+                //todo: navigate to news view
+              },
+              child: const Text('Log in'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _provideTextField(String label, TextEditingController controller, bool obscure) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscure,
+        decoration: InputDecoration(border: const OutlineInputBorder(), labelText: label),
+      ),
+    );
+  }
+
+}
