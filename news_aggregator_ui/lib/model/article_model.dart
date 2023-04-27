e05b259c7632292a4cab656ed5886b1e6c78b236
Mar <up2079248@myport.ac.uk>
2023-04-27 17:21:01 +0100

    Created the domain of article

---
+++ news_aggregator_ui/lib/model/article_model.dart
@@ -0,0 +1,17 @@
+class ArticleModel {
+  final int id;
+  final String author;
+  final String source;
+  final String title;
+  final String url;
+  // final DateTime day;
+
+  ArticleModel({
+    required this.id,
+    required this.author,
+    required this.source,
+    required this.title,
+    required this.url,
+    // required this.day,
+  });
+}
