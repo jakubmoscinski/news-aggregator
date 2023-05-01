ce8d0e735ee5327a1f15a23dd16bb604aa297405
Prasid Pun <up2018399@myport.ac.uk>
2023-05-01 09:37:38 +0100

    Issue_012_ui: News service

---
+++ news_aggregator_ui/lib/service/article_service.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:news_aggregator_ui/controller/controller.dart';
+import 'package:news_aggregator_ui/model/article_model.dart';
+
+class ArticleService with ChangeNotifier {
+  static const _timeout = 8;
+  final Controller _controller;
+
+  List<ArticleModel> _articles = [];
+
+  ArticleService() : _controller = const Controller(baseUrl: 'http://10.0.2.2:8080/news-aggregator/article');
+
+  Future<void> fetchData() async {
+    final clientResponse = await _controller.getData(urlPart: '/list', timeout: _timeout);
+
+    _articles.clear();
+
+    if (clientResponse.isSuccess) {
+      final List<ArticleModel> articles = [];
+
+      List<dynamic> responseList = clientResponse.response;
+      for (var response in responseList) {
+        final _article = ArticleModel(
+          id: response['id'],
+          author: response['author'],
+          source: response['source'],
+          title: response['title'],
+          url: response['url'],
+          // day: model['publishedAt'],
+        );
+
+        articles.add(_article);
+      }
+
+      _articles.addAll(articles);
+    } else {
+      debugPrint(clientResponse.errorMessage);
+    }
+
+    notifyListeners();
+  }
+
+  List<ArticleModel> get articles => _articles;
+}
