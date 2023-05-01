87e60b895280d3198fa2ce7ecf9562847dc78d21
Prasid Pun <up2018399@myport.ac.uk>
2023-05-01 09:49:06 +0100

    Issue_013_ui: News view - api connection - single scroll view

--- news_aggregator_ui/lib/view/news_view.dart
+++ news_aggregator_ui/lib/view/news_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_aggregator_ui/model/article_model.dart';
+import 'package:news_aggregator_ui/service/article_service.dart';
+import 'package:provider/provider.dart';
 
 class NewsView extends StatelessWidget {
   const NewsView({super.key});
@@ -23,30 +26,50 @@ class NewsView extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: ListView(
-            children: const [
-              Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text("Article 1"),
-              ),
-              Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text("Article 2"),
-              ),
-              Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text("Article 3"),
-              ),
-              Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text("Article 4"),
-              ),
-            ]
-        ),
-      )
+        child: ChangeNotifierProvider(
+            create: (_) => ArticleService(),
+            builder: (context, child) {
+              return Consumer<ArticleService>(builder: (context, value, child) {
+                context.read<ArticleService>().fetchData();
+
+                return Center(
+                  child: ListView(
+                    children: _formatArticles(value.articles)
+                  ),
+                );
+              });
+            }),
+      ),
     );
   }
 
+  List<Widget> _formatArticles(List<ArticleModel> articles) {//todo further formatting
+    List<Padding> paddings = [];
 
+    for (ArticleModel article in articles) {
+      final padding = Padding(
+        padding: const EdgeInsets.all(10),
+        child: Container(
+          color: Colors.white10,
+          child: ListTile(
+            contentPadding: const EdgeInsets.all(5),
+            dense: true,
+            onTap: () async {
+
+            },
+            title: Text(
+              article.title,
+              style: const TextStyle(fontSize: 20),
+            ),
+            trailing: const Icon(Icons.bookmark),
+          ),
+        ),
+      );
+
+      paddings.add(padding);
+    }
+
+    return paddings;
+  }
 
 }
