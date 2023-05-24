7f9199dc6bd4e6e40a5305982f4c2ea4331ec270
Jakub Mościński <jakub.moscinski@jwmenergia.pl>
2023-05-24 15:16:50 +0100

    Issue 009 s: Repositories adjusted - new queries added

--- news-aggregator-server/flows/src/main/java/news/aggregator/flows/repository/ArticleRepository.java
+++ news-aggregator-server/flows/src/main/java/news/aggregator/flows/repository/ArticleRepository.java
@@ -1,13 +1,24 @@
 package news.aggregator.flows.repository;
 
 import news.aggregator.flows.model.Article;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    @Query("SELECT a FROM Article a WHERE a.id = :id")
+    Optional<Article> findArticleById(@Param("id") final long id);
+
+    @Query("SELECT a FROM Article a ORDER BY a.id DESC")
+    List<Article> findArticles(Pageable pageable);
 
 }
--- news-aggregator-server/flows/src/main/java/news/aggregator/flows/repository/MemberRepository.java
+++ news-aggregator-server/flows/src/main/java/news/aggregator/flows/repository/MemberRepository.java
@@ -2,12 +2,18 @@ package news.aggregator.flows.repository;
 
 import news.aggregator.flows.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("SELECT m FROM Member m WHERE m.username = :username")
+    Optional<Member> getMemberByUsername(@Param("username") final String username);
 
 }
