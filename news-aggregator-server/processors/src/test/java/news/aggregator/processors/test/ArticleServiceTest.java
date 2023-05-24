977a13749a8e15e6b8ee31aa5926e47351c463e6
Jakub Mościński <jakub.moscinski@jwmenergia.pl>
2023-05-24 15:28:47 +0100

    Issue #012 Unit tests - Flows and Processors modules covered

---
+++ news-aggregator-server/processors/src/test/java/news/aggregator/processors/test/ArticleServiceTest.java
@@ -0,0 +1,71 @@
+package news.aggregator.processors.test;
+
+import news.aggregator.flows.model.Article;
+import news.aggregator.flows.service.ArticleService;
+import news.aggregator.processors.Application;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootTest(classes = { Application.class })
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class ArticleServiceTest {
+
+    private final ArticleService service;
+
+    @Autowired
+    public ArticleServiceTest(@Qualifier("articleService") final ArticleService service) {
+        this.service = service;
+    }
+
+    @Test
+    void test01SaveMember() {
+        this.service.dropTableContent();
+
+        final Article article = new Article();
+        article.setAuthor("Author");
+        article.setSource("Source");
+        article.setTitle("Title");
+        article.setUrl("Url");
+        article.setPublicationDay(LocalDate.now());
+
+        final Article savedArticle = this.service.saveArticle(article);
+
+        assert Objects.nonNull(savedArticle);
+
+        assert article.getAuthor().equals(savedArticle.getAuthor());
+        assert article.getSource().equals(savedArticle.getSource());
+        assert article.getTitle().equals(savedArticle.getTitle());
+        assert article.getUrl().equals(savedArticle.getUrl());
+        assert article.getPublicationDay().equals(savedArticle.getPublicationDay());
+    }
+
+    @Test
+    void test02FindArticles() {
+        final List<Article> articles = this.service.findArticles();
+
+        assert articles.size() > 0;
+    }
+
+    @Test
+    void test03FindArticleById() {
+        final List<Article> articles = this.service.findArticles();
+
+        if (articles.size() > 0) {
+            final Article article = articles.get(0);
+
+            assert Objects.nonNull(this.service.findArticleById(article.getId()));
+        } else {
+            assert false;
+        }
+
+    }
+
+}
---
+++ news-aggregator-server/processors/src/test/java/news/aggregator/processors/test/NewsApiSchedulerTest.java
@@ -0,0 +1,42 @@
+package news.aggregator.processors.test;
+
+import news.aggregator.flows.model.Article;
+import news.aggregator.processors.Application;
+import news.aggregator.processors.controller.NewsApiController;
+import news.aggregator.processors.domain.NewsApiResponse;
+import news.aggregator.processors.processor.NewsApiProcessor;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootTest(classes = { Application.class })
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class NewsApiSchedulerTest {
+
+    private final NewsApiController controller;
+    private final NewsApiProcessor processor;
+
+    @Autowired
+    public NewsApiSchedulerTest(@Qualifier("newsApiController") final NewsApiController controller,
+                                @Qualifier("newsApiProcessor") final NewsApiProcessor processor) {
+        this.controller = controller;
+        this.processor = processor;
+    }
+
+    @Test
+    void test01schedule() {
+        final NewsApiResponse response = this.controller.getNews();
+        assert Objects.nonNull(response);
+        assert response.getArticles().size() > 0;
+
+        final List<Article> articles = this.processor.apply(response);
+        assert articles.size() > 0;
+    }
+
+}
